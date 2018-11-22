b14ea75ae3b776644cfbd1dc68c9132ca83f1659
Jiaqi Wang <1400012887@pku.edu.cn>
2018-11-22 11:24:12 +0000

    Base structure updated by Jiaqi Wang

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def help
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,35 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
   get 'welcome/index'
+
   resources :articles do
     resources :comments
   end
+
   root 'welcome#index'
+
+  get '/contact', to: 'welcome#contact'
+
+  get '/about', to: 'welcome#about'
+
+  get '/help', to: 'welcome#help'
+
+  get  '/signup',  to: 'users#new'
+
+  post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+
+  post   '/login',   to: 'sessions#create'
+
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
